50bf8885ece64db1485671433453abc9785c92f0
sbaof <184040280@qq.com>
2017-04-21 15:05:23 +0800

    msg

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -17,14 +17,38 @@ class WechatController extends Controller
      */
     public function serve()
     {
-        // Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-       $wechat = app('wechat');
+        $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
-            return "啦啦啦，辣鸡";
-        }); 
-
-        // Log::info('return response.');
+        	 switch ($message->MsgType) {
+		        case 'event':
+		            return '哈哈哈哈';
+		            break;
+		        case 'text':
+		            return 'foo';
+		            break;
+		        case 'image':
+		            return '收到图片消息';
+		            break;
+		        case 'voice':
+		            return '收到语音消息';
+		            break;
+		        case 'video':
+		            return '收到视频消息';
+		            break;
+		        case 'location':
+		            return '收到坐标消息';
+		            break;
+		        case 'link':
+		            return '收到链接消息';
+		            break;
+		        // ... 其它消息
+		        default:
+		            return '收到其它消息';
+		            break;
+		    }
+            // return "啦啦啦，辣鸡";
+        });
 
         return $wechat->server->serve();
     }
